0ae126ff93b3f0eedc901c5b7ced0334dfa84f10
Omer Nadeem <omernadeem3@gmail.com>
2017-03-11 17:24:59 -0500

    feat(phaser game starting concurrently on multiple clients)

--- client/src/sockets.js
+++ client/src/sockets.js
@@ -1,12 +1,17 @@
 import store from './store.js';
-import { loadPlayers, setCurrentPlayer } from './reducers/players-reducer.js';
+import { loadPlayers, setCurrentPlayer, changeGamePlaying } from './reducers/players-reducer.js';
 import { loadMessages, addMessage } from './reducers/chatApp-reducer.js';
+
+import BootState from './gameStates/boot.js';
+import PreloadState from './gameStates/preload.js';
+import MiniGameOneState from './gameStates/minigame1/gameState.js';
 
 
 const attachFunctions = (socket) => {
   socket.on('playerUpdate', dispatchPlayerUpdates);
   socket.on('currentPlayer', dispatchCurrentPlayer);
   socket.on('messagesUpdate', dispatchNewMessage)
+  socket.on('gameIsStarting', dispatchNewGame)
 };
 
 function dispatchPlayerUpdates(players) {
@@ -26,5 +31,17 @@ export function dispatchNewMessage(msgObj) {
   console.log('Store will receive a new message:', msgObj);
   store.dispatch(addMessage(msgObj));
 }
+
+function dispatchNewGame(players) {
+  store.dispatch(changeGamePlaying(true))
+
+  PB.game = new Phaser.Game('100%', '100%', Phaser.AUTO, 'game');
+  PB.game.state.add('Boot', BootState);
+  PB.game.state.add('Preload', PreloadState);
+  PB.game.state.add('MiniGameOne', MiniGameOneState);
+  PB.game.state.start('Boot', true, false, players);
+}
+
+function dispatchNewGameState(){}
 
 export default attachFunctions;
